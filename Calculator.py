7dcccda1a49e2e2976ad0f1baf58ccdcd20f1376
BobbyYan-Burnside <21589@burnside.school.nz>
2025-02-13 19:56:49 +1300

    add buttons

--- Calculator.py
+++ Calculator.py
@@ -14,8 +14,46 @@ class Calculator:
         self.WidgetFrame = Frame(self.MainFrame, bd =18, width = 590, height=600, relief=RIDGE, bg = 'cadet blue')
         self.WidgetFrame.grid()
 
+        self.lbDisplay = Label(self.WidgetFrame, width =30, height=2, bg='white', font=('arial', 20, 'bold'), anchor='e')
+        self.lbDisplay.grid (row =0, column=0, columnspan=4, padx=10, pady=10)
+        
+        self.input_button = ""
+
+        self.create_button("←", 1, 0)
+        self.create_button("CE",1 ,1)
+        self.create_button("C", 1, 2)
+        self.create_button("±", 1, 3)
+
+        self.create_button("7", 2, 0)
+        self.create_button("8", 2, 1)
+        self.create_button("9", 2, 2)
+        self.create_button("+", 2, 3)
+        
+        self.create_button("4", 3, 0)
+        self.create_button("5", 3, 1)
+        self.create_button("6", 3, 2)
+        self.create_button("-", 3, 3)
+
+        self.create_button("1", 4, 0)
+        self.create_button("2", 4, 1)
+        self.create_button("3", 4, 2)
+        self.create_button("*", 4, 3)
+
+        self.create_button("0", 5, 0)
+        self.create_button(".", 5, 1)
+        self.create_button("=", 5, 2)
+        self.create_button("/", 5, 3)
+
+
+    def create_button(self, text, row, column):
+            btnWidget = Button(self.WidgetFrame, text=text, width =6, height=2, bd=4 , bg='powder blue', font =('arial', 20, 'bold'))
+            btnWidget.grid(row =row, column =column , padx=5, pady =5)
+
 
 
 root = Tk()
 App = Calculator(root)
 root.mainloop()
+
+
+
